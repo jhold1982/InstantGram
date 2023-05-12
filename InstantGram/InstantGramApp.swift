ef239e83cb2606741279a0d19e593c8ce09863d6
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-05-12 11:32:36 -0500

    Initial Commit

---
+++ InstantGram/InstantGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantGramApp.swift
+//  InstantGram
+//
+//  Created by Justin Hold on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstantGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
